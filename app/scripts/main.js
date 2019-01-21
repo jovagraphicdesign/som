bbf5f3f22e4752c8f47c91e7ee626f5f9272bf66
johan Vanelderen <vanelderen.johan@telenet.be>
2019-01-21 07:04:41 +0100

    forms aangepast,privacybeleid

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('#checkGDPRinschrijven').click(function () {
+    console.log('clicked')
+      $('#completeInschrijven').prop('disabled', !$('#checkGDPRinschrijven').prop('checked'))
+  })
+
+  $('#gdprChecksolliciteer').click(function () {
+    console.log('clicked')
+      $('#completeSolliciteer').prop('disabled', !$('#gdprChecksolliciteer').prop('checked'))
+  })
+
+  $('#gdprCheckContact').click(function () {
+    console.log('clicked')
+      $('#completeContact').prop('disabled', !$('#gdprCheckContact').prop('checked'))
+  })  
+
+})
