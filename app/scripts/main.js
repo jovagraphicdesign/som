a13c437edaffba80e6829718e179fb1067ac25df
johan Vanelderen <vanelderen.johan@telenet.be>
2019-01-21 13:15:34 +0100

    indent 4tap to 2space

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
   $('#checkGDPRinschrijven').click(function () {
     console.log('clicked')
-      $('#completeInschrijven').prop('disabled', !$('#checkGDPRinschrijven').prop('checked'))
+    $('#completeInschrijven').prop('disabled', !$('#checkGDPRinschrijven').prop('checked'))
   })
 
   $('#gdprChecksolliciteer').click(function () {
     console.log('clicked')
-      $('#completeSolliciteer').prop('disabled', !$('#gdprChecksolliciteer').prop('checked'))
+    $('#completeSolliciteer').prop('disabled', !$('#gdprChecksolliciteer').prop('checked'))
   })
 
   $('#gdprCheckContact').click(function () {
     console.log('clicked')
-      $('#completeContact').prop('disabled', !$('#gdprCheckContact').prop('checked'))
-  })  
+    $('#completeContact').prop('disabled', !$('#gdprCheckContact').prop('checked'))
+  })
 
 })
